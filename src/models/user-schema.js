b23f1b418c20de70ff501d400bda47cf5aa355ac
Sergey Vladimirov <vladimirov.exceedteam@gmail.com>
2022-04-26 12:44:34 +0300

    fix

--- src/models/user-schema.js
+++ src/models/user-schema.js
@@ -1,7 +1,17 @@
-const { Schema, model } = require('mongoose');
+const {
+  Schema,
+  model,
+} = require("mongoose");
 const User = new Schema({
-  email: { type: String, unique: true, required: true },
-  password: { type: String, required: true }
-})
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
 
-module.exports = model('User', User);
+module.exports = model("User", User);
